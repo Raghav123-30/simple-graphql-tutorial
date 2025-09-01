152f68e1c1ce636e1b72117e6917d938c17d298a
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-09-01 16:31:18 +0530

    Explore simpler graphql queries in server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
 import express from "express";
 
 dotenv.config();
@@ -9,8 +10,33 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello there!" });
 });
 
+const typeDefs = `#graphql
+ 
+ type Student{
+   name:String
+ }
+  
+ type Query{
+  students:[Student]
+ }
+
+`;
+
+const resolvers = {
+  Query: {
+    students: () => [
+      { name: "Raghavendra" },
+      { name: "Samant" },
+      { name: "Puneet" },
+    ],
+  },
+};
+
 const startServer = async () => {
   const PORT = process.env.PORT || 3000;
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  app.use("/graphql", express.json(), expressMiddleware(server));
   app.listen(PORT, () => {
     console.log(`🚀 Server running  at http://localhost:${PORT}`);
   });
