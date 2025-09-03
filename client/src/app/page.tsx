0c5f38829f8ee91e93fac2cdf47f9138cf97282b
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-09-03 19:04:10 +0530

    Explore performing graphql mutation in client side react components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
+import { useMutation, useQuery } from "@apollo/client/react";
 
 type Todo = {
   id: number;
@@ -18,7 +18,20 @@ export default function Home() {
       }
     }
   `;
+  const UPDATE_TODO = gql`
+    mutation updateTodo($id: ID!) {
+      updateTodo(id: $id) {
+        id
+        title
+        done
+      }
+    }
+  `;
   const { data, error, loading } = useQuery<{ todos: Todo[] }>(TODOS_QUERY);
+  const [updateTodo, { loading: updating, error: errorUpdating }] =
+    useMutation<{ updatedTodo: Todo }>(UPDATE_TODO, {
+      refetchQueries: [TODOS_QUERY],
+    });
   if (loading) {
     return <p>loading...</p>;
   }
@@ -31,6 +44,18 @@ export default function Home() {
         <div key={item.id} className="bg-gray-50 p-3 rounded-md shadow-md">
           <h1 className="font-bold text-lg">{item.title}</h1>
           <p className="text-gray-600">{item.done ? "Completed" : "Pending"}</p>
+          <button
+            onClick={() => updateTodo({ variables: { id: item.id } })}
+            disabled={updating}
+            className={`${item.done ? "text-red-500" : "text-green-500"}`}
+          >
+            {updating
+              ? "Updating"
+              : item.done
+              ? "Mark as pending"
+              : "Mark as complete"}
+          </button>
+          {errorUpdating && <p className="text-red-500">Failed to update</p>}
         </div>
       ))}
     </div>
